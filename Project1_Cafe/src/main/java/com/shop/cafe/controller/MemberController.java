b272a8dda10a98a8ddd16bb8b34ae13536a43ee8
udwns310 <uroasecret5@gmail.com>
2025-03-06 16:10:02 +0900

    first commit

---
+++ Project1_Cafe/src/main/java/com/shop/cafe/controller/MemberController.java
@@ -0,0 +1,61 @@
+package com.shop.cafe.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.shop.cafe.dto.Member;
+import com.shop.cafe.service.MemberService;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+
+@RestController
+//@CrossOrigin("http://127.0.0.1:8080/")
+public class MemberController {
+	
+	@Autowired
+	MemberService memberService;
+	
+	@PostMapping("login")
+	public Map<String, String> login(@RequestBody Member m, HttpServletRequest request) {
+		Map<String,String> responseData=new HashMap<String, String>();
+		try {
+			m=memberService.login(m);
+			if(m!=null) {//login ok
+				HttpSession session=request.getSession();
+				System.out.println(session.getId());
+				session.setAttribute("member", m);
+				responseData.put("msg","ok");
+			}else {//login fail
+				responseData.put("msg","다시 로그인 해주세요");
+			}
+		} catch (Exception e) {//login error			
+			e.printStackTrace();
+			responseData.put("msg","다시 로그인 해주세요");
+		}
+		return responseData;
+	}
+	
+	@PostMapping("insertMember")
+	public Map<String, String> insertMember(@RequestBody  Member m) {
+		Map<String,String> responseData=new HashMap<String, String>();
+		try {
+			memberService.insertMember(m);
+			responseData.put("msg","ok");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			responseData.put("msg",e.getMessage());
+		}
+		
+		return responseData;
+	}
+	
+
+}
---
+++ Project1_Cafe/src/main/java/com/shop/cafe/controller/ProductController.java
@@ -0,0 +1,31 @@
+package com.shop.cafe.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.shop.cafe.dto.Product;
+import com.shop.cafe.service.ProductService;
+
+@RestController
+//@CrossOrigin("http://127.0.0.1:8080/")
+public class ProductController {
+	
+	@Autowired
+	ProductService productService;
+	
+	@GetMapping("getAllProducts")
+	public List<Product> getAllProducts() {
+		try {
+			return productService.getAllProducts();
+		}catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+		
+		
+	}
+}
